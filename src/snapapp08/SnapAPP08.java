37043d1055d5243c18b6c1be0ea26787940bbd51
MochamadFandi <mfandi@student.ce.undip.ac.id>
2018-10-29 08:00:52 +0700

    upload pertama

---
+++ src/snapapp08/SnapAPP08.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snapapp08;
+
+/**
+ *
+ * @author Admin
+ */
+public class SnapAPP08 extends SnapFrame{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SnapAPP08 snap = new SnapAPP08();
+        snap.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
